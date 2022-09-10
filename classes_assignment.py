609229febebd3ca08a04474c244434c62a912fbc
Kari Fuller <kari.christine.fuller@gmail.com>
2022-09-10 10:52:37 -0700

    parent/child classes

---
+++ classes_assignment.py
@@ -0,0 +1,14 @@
+class Family: #parent class
+    first_name = 'First Name'
+    last_name = 'Last Name'
+    phone = ' '
+    email = ' ' 
+
+class Mother(Family): #child class will have this info and family info
+    maiden_name = 'Maiden Name'
+    drivers_license = ' '
+
+class Brother(Family): #child class
+    middle_name = 'Middle Name' 
+    age = ' ' 
+    
