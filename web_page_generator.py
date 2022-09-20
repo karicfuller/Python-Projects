5e71a4dc87fc56e7da477b9bfaa4f53a6d3330f9
Kari Fuller <kari.christine.fuller@gmail.com>
2022-09-20 16:55:39 -0700

    tested/complete

--- web_page_generator.py
+++ web_page_generator.py
@@ -1,13 +1,9 @@
 
 
-
-
 import tkinter as tk
 from tkinter import *
 import webbrowser
 import os
-
-
 
 
 class ParentWindow(Frame):
@@ -15,48 +11,39 @@ class ParentWindow(Frame):
         Frame.__init__(self, master)
         self.master.title('Web Page Generator')
 
-        self.lbl = Label(text='Enter custom text or click the Default HTML page button')
+        # label above entry field
+        self.lbl = Label(text='Enter custom text or click the Default HTML page button') 
         self.lbl.grid(row=0, column=0, padx=(20), pady=(20,0))
 
-        self.enter_info = Entry(self.master)
+        # entry field
+        self.enter_info = Entry(self.master) 
         self.enter_info.grid(row=1, column=0, columnspan=3, padx=(30,15), pady=(10,10), sticky=W+E)  
 
-        self.btn = Button(self.master, text='Default HTML Page', width=20, height=1, command=self.defaultHTML)
-        self.btn.grid(row=2, column=1, padx=(0,10), pady=(0,10))
+        # default button
+        self.default_btn = Button(self.master, text='Default HTML Page', width=20, height=1, command=self.defaultHTML)
+        self.default_btn.grid(row=2, column=1, padx=(0,10), pady=(0,10))
 
+        # submit button
         self.submit_btn = Button(self.master, text='Submit Custom Text', width=20, height=1, command=self.entryHTML)
         self.submit_btn.grid(row=2, column=2, padx=(0,10), pady=(0,10))
 
-        
 
     def defaultHTML(self):
-        print("Hello")
-        htmlText = 'Stay Tuned for Our Amazing Summer Sale!'
-        htmlFile = open('index.html', 'w')
+        htmlText = 'You Couldn\'t Think of Anything to Type Inside the Box?' # verbiage if default button is clicked
+        htmlFile = open('index.html', 'w') # w=write only
         htmlContent = '<html>\n<body>\n<h1>' + htmlText + '</h1>\n</body>\n</html>'
         htmlFile.write(htmlContent)
         htmlFile.close()
-        #webbrowser.open_new_tab("index.html")
         webbrowser.get().open_new_tab('file:///' + os.getcwd() + '/index.html')
 
     def entryHTML(self):
-        htmlText = self.enter_info.get()
-        htmlFile = open('index.html', 'w')
+        htmlText = self.enter_info.get() # verbiage entered into field
+        htmlFile = open('index.html', 'w') 
         htmlContent = '<html>\n<body>\n<h1>' + htmlText + '</h1>\n</body>\n</html>'
         htmlFile.write(htmlContent)
         htmlFile.close()
-        #webbrowser.open_new_tab("index.html")
         webbrowser.get().open_new_tab('file:///' + os.getcwd() + '/index.html')
                       
-
-
-
-
-
-
-
-
-
         
 
 if __name__ == "__main__":
